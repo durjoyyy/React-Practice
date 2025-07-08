738dd8933bf75d5cb2838bb4ca010a6c816a30c7
Durjoy Ghosh <durjoy12091999@gmail.com>
2025-07-08 23:20:58 +0530

    first setup, counter

---
+++ 02Counter/counter/src/App.jsx
@@ -0,0 +1,31 @@
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
+
+function App() {
+  let currentCount = 5;
+ const [count,setCount] =useState(0);
+
+  const addValue = () => {
+    setCount(count+1);
+  };
+
+  const substractValue = () => {
+    setCount(count-1);
+  };
+
+  return (
+    <>
+      <h1>Durjoy GG</h1>
+      <h2>Counter Value: {count}</h2>
+
+      <button onClick={addValue}>Add Value</button>
+      <br />
+      <br />
+      <button onClick={substractValue}>Substract Value</button>
+    </>
+  );
+}
+
+export default App;
